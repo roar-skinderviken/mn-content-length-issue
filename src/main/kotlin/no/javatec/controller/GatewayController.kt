0d9357169eec6775ac56cf27d3d9a2894d8e77f7
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-07-29 21:22:00 +0200

    micronaut-parent:4.0.2

--- src/main/kotlin/no/javatec/controller/GatewayController.kt
+++ src/main/kotlin/no/javatec/controller/GatewayController.kt
@@ -1,7 +1,6 @@
 package no.javatec.controller
 
 import io.micronaut.http.HttpRequest
-import io.micronaut.http.MediaType
 import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
@@ -25,6 +24,6 @@ class GatewayController(
      * @param request [HttpRequest] instance.
      * @return [Publisher] of [MutableHttpResponse].
      */
-    @Get(value = "/health/liveness", produces = [MediaType.APPLICATION_JSON])
+    @Get(value = "/health/liveness")
     fun barnBvrApiGetStatus(request: HttpRequest<*>): Publisher<MutableHttpResponse<*>> = proxyRequest(request)
 }
