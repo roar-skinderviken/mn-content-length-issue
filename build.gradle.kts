400e59230a046473a4b07cf700feca06d8a7a841
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-06-02 20:11:21 +0200

    Maven -> Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+    id("io.micronaut.application") version "4.4.0"
+}
+
+version = "0.1"
+group = "no.javatec"
+
+val kotlinVersion= project.properties["kotlinVersion"]
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    runtimeOnly("org.yaml:snakeyaml")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-management")
+    implementation("io.projectreactor:reactor-core:3.6.5")
+    testImplementation("io.micronaut.test:micronaut-test-kotest5")
+}
+
+application { mainClass = "no.javatec.ApplicationKt" }
+kotlin { jvmToolchain(21) }
+
+micronaut {
+    runtime("netty")
+    testRuntime("kotest5")
+    processing {
+        incremental(true)
+        annotations("no.javatec.*")
+    }
+}
+
+
+
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name="micronaut-proxy-http-client-demo"
